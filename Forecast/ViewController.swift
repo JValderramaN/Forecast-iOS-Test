10d631872c708ef4b978ebbf58690c039810a7e4
José Angel Valderrama Noguez <josevalderrama18@gmail.com>
2018-04-16 12:21:11 -0300

    Initial Commit

---
+++ Forecast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Forecast
+//
+//  Created by José Valderrama on 4/16/18.
+//  Copyright © 2018 José Valderrama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ForecastTests/ForecastTests.swift
@@ -0,0 +1,36 @@
+//
+//  ForecastTests.swift
+//  ForecastTests
+//
+//  Created by José Valderrama on 4/16/18.
+//  Copyright © 2018 José Valderrama. All rights reserved.
+//
+
+import XCTest
+@testable import Forecast
+
+class ForecastTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
